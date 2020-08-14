044998e2c0bad87bfef7da9c777e08258b5eb19e
MikChanna <channa.mik@gmail.com>
2020-08-13 21:15:21 -0400

    added inquirer prompt to view employees

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ var connection = mysql.createConnection({
   database: "employee_tracker",
 });
 
+// calls function to access sql tables
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id" + connection.threadID + "\n");
-  readEmployees();
+  console.log("You are now connected");
+  promptUser();
+  //   readEmployees();
+  //   financeEmployees();
 });
 
+// Displays all employees
 function readEmployees() {
   console.log("Selecting all employees");
   connection.query("SELECT * FROM employee", function (err, res) {
@@ -24,4 +28,38 @@ function readEmployees() {
     console.log(res);
     connection.end();
   });
+}
+
+// function financeEmployees() {
+//   console.log("Selecting all finance employees");
+//   connection.query(("SELECT FROM employee WHERE department_id" = 1), function (
+//     err,
+//     res
+//   ) {
+//     if (err) throw err;
+//     console.log(res);
+//     connection.end();
+//   });
+// }
+
+function promptUser(answers) {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+          "View Employees",
+          "Add Employee",
+          "Delete Employee",
+          "Edit Employee",
+        ],
+      },
+    ])
+    .then((answers) => {
+      if (answers.action === "View Employees") {
+        readEmployees();
+      }
+    });
 }
