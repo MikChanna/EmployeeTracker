c9a5a5371ee239d6f41696772dd0c80a224b5d04
MikChanna <channa.mik@gmail.com>
2020-08-13 20:38:18 -0400

    created connection with credentials

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const pw = require("./pw");
+
+// create sql connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: pw,
+  database: "employee_tracker",
+});
