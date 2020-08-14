8909627ace304686982b07d1e24194122ce30754
MikChanna <channa.mik@gmail.com>
2020-08-13 20:46:47 -0400

    created function to select all employees

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const pw = require("./pw");
 
 // create sql connection
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: pw,
   database: "employee_tracker",
 });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadID + "\n");
+  readEmployees();
+});
+
+function readEmployees() {
+  console.log("Selecting all employees");
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
